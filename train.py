5a44b64484d30eaa49c6025afcb2b4c2f9086beb
anlycchen <kimythanly@gmail.com>
2021-04-27 21:37:15 +0800

    fire

--- train.py
+++ train.py
@@ -1,28 +1,13 @@
 import os
 import logging
-import importlib
-from glob import glob
 
 import fire
+
+from implementation import get_valid_modules
 
 logging.basicConfig(level=logging.INFO,
                     format='[%(levelname)s] %(asctime)-s %(name)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-
-INVALID_MODULES = {'dataset', 'preprocess', 'util', 'vocoder', 'classifier'}
-
-
-def get_valid_modules():
-    modules = glob(os.path.join('implementation', '*'))
-    valid_modules = {}
-    for module in modules:
-        if os.path.isdir(module):
-            basename = os.path.basename(module)
-            if not basename.startswith('_') and basename not in INVALID_MODULES:
-                valid_modules[basename] = importlib.import_module(
-                    f'.{basename}.train', 'implementation').main
-    return valid_modules
 
 
 def main():
